444168f640b11599313bc9655f8f62fb24e9b69a
ubadineke <ubadinekeprince@gmail.com>
2024-09-13 16:18:23 +0100

    using the Box and if let

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,13 +12,13 @@ fn main() {
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-
-    // dbg!(args);
+    
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
 
 struct Config{
     query: String, 
@@ -34,4 +35,11 @@ impl Config{
 
         Ok(Config {query, file_path})
     }
+}
+
+fn run (config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
